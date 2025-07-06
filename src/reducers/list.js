80e24258646c782830f3eac77c9f060bed9c45a4
Barun Kanti Das <barunbasis37@gmail.com>
2025-07-06 10:00:45 +0600

    list reducer updated

--- src/reducers/list.js
+++ src/reducers/list.js
@@ -1,11 +1,40 @@
 export const listReducer=(lists=[], action)=>{
 
     switch (action.type){
-        case "CREATE_LIST":{}
-        case "UPDATE_LIST_NMAE":{}
+        case "CREATE_LIST":{
+
+            const newList={
+                id: action.payload.id,
+                title: action.payload.title,
+                boardId: action.payload.boardId,
+                task: [],  
+            };
+            return[...lists, newList];
+
+        }
+        case "UPDATE_LIST_NAME":{
+
+            const updatedLists= lists.map((item)=>{
+                if(item.id=action.payload.id){
+                    return{
+                        ...item,
+                        title: action.payload.title
+                    };
+                }
+                return item;
+
+            });
+            return updatedLists;
+        }
         case "CHANGE_BOARD_ID":{}
         case "REMOVE_LIST":{}
-        
+        case "ADD_TASK_TO_A_LIST":{}
+        case "REMOVE_TASK_FROM_A_LIST":{}
+        default:{
+            return lists;
+        }
+
+
     }
 
 }
